98416476f474ac8117f82224afec54647f30ac69
ParraJesus <jgparra@unicauca.edu.co>
2024-12-09 10:05:33 -0500

    grafica final

--- src/Components/Graph3d.jsx
+++ src/Components/Graph3d.jsx
@@ -3,11 +3,12 @@ import { Canvas } from "@react-three/fiber";
 import { Line, Text } from "@react-three/drei";
 import { evaluate } from "mathjs";
 import { OrbitControls } from "@react-three/drei";
+import { PlaneGeometry, MeshBasicMaterial, Mesh } from "three";
 
+/*
 const Axes = () => {
   return (
     <>
-      {/* Eje X */}
       <Line
         points={[
           [-15, 0, 0],
@@ -25,7 +26,6 @@ const Axes = () => {
       >
         X
       </Text>
-      {/* Eje Y */}
       <Line
         points={[
           [0, -15, 0],
@@ -43,7 +43,6 @@ const Axes = () => {
       >
         Y
       </Text>
-      {/* Eje Z */}
       <Line
         points={[
           [0, 0, -10],
@@ -56,12 +55,12 @@ const Axes = () => {
   );
 };
 
-const Wave = ({ color, formula, axis, label }) => {
+const Wave = ({ color, formula, axis, label, l }) => {
   const points = [];
   const positionLabel = axis === "x" ? [5, 4, 0] : [5, 2, 0];
 
   // Generar puntos basados en la fórmula
-  for (let z = 0; z <= 20; z += 0.1) {
+  for (let z = 0; z <= l; z += 0.1) {
     let displacement = 0;
     try {
       displacement = evaluate(formula, { z });
@@ -79,7 +78,6 @@ const Wave = ({ color, formula, axis, label }) => {
   return (
     <>
       <Line points={points} color={color} lineWidth={2} />
-      {/* Etiqueta de la onda */}
       <Text
         position={positionLabel}
         fontSize={1}
@@ -93,7 +91,12 @@ const Wave = ({ color, formula, axis, label }) => {
   );
 };
 
-const ElectromagneticWave = () => {
+const ElectromagneticWave = ({
+  formulas,
+  cavityLenght,
+  harmonicNumber,
+  closedData,
+}) => {
   const phaseRef = useRef(0);
   const [phase, setPhase] = useState(0);
 
@@ -101,10 +104,10 @@ const ElectromagneticWave = () => {
   const [lenght, setLenght] = useState(0); //x
 
   const [electricFieldFormula, setElectricFieldFormula] = useState(
-    "2 * sin((2 * pi / 5) * z - phase)"
+    "sin((pi / 5) * z - phase)"
   );
   const [magneticFieldFormula, setMagneticFieldFormula] = useState(
-    "2 * sin((2 * pi / 5) * z - phase)"
+    "sin((pi / 5) * z - phase)"
   );
 
   // Animar la fase
@@ -125,27 +128,230 @@ const ElectromagneticWave = () => {
             <ambientLight intensity={0.1} />
             <pointLight position={[10, 10, 10]} />
 
-            {/* Ejes */}
             <Axes />
 
-            {/* Campo eléctrico */}
             <Wave
               color="#df7126"
               formula={electricFieldFormula.replace("phase", phase)}
               axis="x"
               label="Campo Eléctrico (E)"
+              l={cavityLenght}
             />
 
-            {/* Campo magnético */}
             <Wave
               color="#632c78"
               formula={magneticFieldFormula.replace("phase", phase)}
               axis="y"
               label="Campo Magnético (B)"
+              l={cavityLenght}
             />
             <OrbitControls />
           </Canvas>
         </div>
+        <p>Fórmula de E: {formulas.electric}</p>
+        <p>Fórmula de B: {formulas.magnetic}</p>
+        <p>Longitud: {cavityLenght}</p>
+        <p>Condiciones de la cavidad: {closedData}</p>
+        <p>Armónico graficado: {harmonicNumber}</p>
+      </div>
+    </>
+  );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#f0f0f0",
+    padding: "10px",
+    margin: "10px",
+  },
+  controls: {
+    marginBottom: "20px",
+    textAlign: "center",
+  },
+  canvasContainer: {
+    border: "2px solid #000",
+    borderRadius: "10px",
+    width: "80%",
+    height: "70vh",
+    overflow: "hidden",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+};
+
+*/
+
+const Axes = () => {
+  return (
+    <>
+      <Line
+        points={[
+          [-15, 0, 0],
+          [15, 0, 0],
+        ]}
+        color="#d95763"
+        lineWidth={2}
+      />
+      <Text
+        position={[16, 0, 0]}
+        fontSize={1}
+        color="#d95763"
+        anchorX="center"
+        anchorY="middle"
+      >
+        X
+      </Text>
+      <Line
+        points={[
+          [0, -15, 0],
+          [0, 15, 0],
+        ]}
+        color="#99e550"
+        lineWidth={2}
+      />
+      <Text
+        position={[0, 16, 0]}
+        fontSize={1}
+        color="#99e550"
+        anchorX="center"
+        anchorY="middle"
+      >
+        Y
+      </Text>
+      <Line
+        points={[
+          [0, 0, -10],
+          [0, 0, 50],
+        ]}
+        color="#639bff"
+        lineWidth={2}
+      />
+    </>
+  );
+};
+
+const Wave = ({ color, formula, axis, label, l, phaseT, phaseX }) => {
+  const points = [];
+  const positionLabel = axis === "x" ? [5, 4, 0] : [5, 2, 0];
+
+  // Generar puntos basados en la fórmula
+  for (let z = 0; z <= l; z += 0.1) {
+    let displacement = 0;
+    try {
+      displacement = evaluate(`${formula}`, {
+        z: phaseX,
+        t: phaseT,
+        x: z,
+      });
+      //console.log(formula);
+    } catch (e) {
+      console.error("Error evaluando fórmula:", e);
+    }
+
+    if (axis === "x") {
+      points.push([displacement, 0, z]); // Onda en X
+    } else if (axis === "y") {
+      points.push([0, displacement, z]); // Onda en Y
+    }
+  }
+
+  return (
+    <>
+      <Line points={points} color={color} lineWidth={2} />
+      <Text
+        position={positionLabel}
+        fontSize={1}
+        color={color}
+        anchorX="middle"
+        anchorY="middle"
+      >
+        {label}
+      </Text>
+    </>
+  );
+};
+
+const PlaneComponent = ({ position, color }) => {
+  return (
+    <mesh position={position}>
+      <planeGeometry args={[15, 15]} />
+      <meshBasicMaterial color={color} opacity={0.3} transparent side={2} />
+    </mesh>
+  );
+};
+
+const ElectromagneticWave = ({
+  formulas,
+  cavityLenght,
+  harmonicNumber,
+  closedData,
+}) => {
+  const [phaseX, setPhaseX] = React.useState(0); // Contador para x
+  const [phaseT, setPhaseT] = React.useState(0); // Contador para t
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setPhaseT((prev) => prev + 0.1); // Actualiza el tiempo
+      setPhaseX((prev) => prev + 0.1); // Actualiza la posición espacial
+    }, 50);
+    return () => clearInterval(interval);
+  }, []);
+
+  const electricField = formulas.electric;
+
+  const magneticField = formulas.magnetic;
+
+  return (
+    <>
+      <div style={styles.container}>
+        <p>La vista tiene navegación</p>
+        <div style={styles.canvasContainer}>
+          <Canvas camera={{ position: [12, 8, 30], rotateY: 50, fov: 60 }}>
+            <ambientLight intensity={0.1} />
+            <pointLight position={[10, 10, 10]} />
+
+            <Axes />
+
+            <Wave
+              color="#df7126"
+              formula={electricField}
+              axis="x"
+              label="Campo Eléctrico (E)"
+              l={cavityLenght}
+              phaseT={phaseT}
+              phaseX={phaseX}
+            />
+
+            <Wave
+              color="#632c78"
+              formula={magneticField}
+              axis="y"
+              label="Campo Magnético (B)"
+              l={cavityLenght}
+              phaseT={phaseT}
+              phaseX={phaseX}
+            />
+
+            {closedData === "abierta-cerrada" && (
+              <PlaneComponent position={[0, 0, cavityLenght]} color="green" />
+            )}
+
+            {closedData === "cerrada-cerrada" && (
+              <>
+                <PlaneComponent position={[0, 0, cavityLenght]} color="green" />
+                <PlaneComponent position={[0, 0, 0]} color="red" />
+              </>
+            )}
+            <OrbitControls />
+          </Canvas>
+        </div>
+        <p>{closedData}</p>
       </div>
     </>
   );
--- src/Components/WaveControls.jsx
+++ src/Components/WaveControls.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import katex from "katex";
 import "katex/dist/katex.min.css"; // Importa los estilos de KaTeX
 import Graph3d from "./Graph3d";
-
+/*
 const WaveControls = () => {
   let k, w, lambda, E0, B0;
   const [boundaryCondition, setBoundaryCondition] = useState("cerrada-cerrada");
@@ -30,14 +30,13 @@ const WaveControls = () => {
         `B(x,t) = ${B0} · Sin(${k.toFixed(2)}x) · Cos(${w.toFixed(2)}t)`,
     },
   };
-  
 
   const [inputs, setInputs] = useState({
     L: "", // cadena vacía en lugar de 1
     v: "", // cadena vacía en lugar de 1
     amplitude: "E",
     nodes: "", // cadena vacía en lugar de 1
-    E0: ""
+    E0: "",
   });
 
   const [harmonics, setHarmonics] = useState([]);
@@ -48,7 +47,6 @@ const WaveControls = () => {
     const newHarmonics = [];
 
     for (let n = 1; n <= nodes; n++) {
-      
       // Calcular el valor de k, w y lambda según la condición de frontera
       switch (boundaryCondition) {
         case "cerrada-cerrada":
@@ -56,7 +54,7 @@ const WaveControls = () => {
           k = (Math.PI * n) / L;
           break;
         case "abierta-cerrada":
-          k = (((2 * n) - 1) * Math.PI) / (2*L);
+          k = ((2 * n - 1) * Math.PI) / (2 * L);
           break;
         default:
           k = 0;
@@ -104,25 +102,28 @@ const WaveControls = () => {
     });
   };
 
-    //funciones que devuelve
-    {
-      harmonics.map(({ n, k, w, lambda, E0, B0 }) => {
-        console.log(
-          `E: ${equations[boundaryCondition].E(k, w, E0)},
-  B: ${equations[boundaryCondition].B(k, w, E0)} `
-        );
-      });
-    }
-  
+  const [graphData, setGraphData] = useState({
+    E: "cola",
+    B: "bola",
+  });
+
   return (
     <div className="container mx-auto p-4">
       <div className="bg-white rounded-lg shadow-md p-6 space-y-6">
-      <Graph3d />
+        <Graph3d
+          formulas={{
+            electric: graphData.E,
+            magnetic: graphData.B,
+          }}
+          cavityLenght={inputs.L}
+          harmonicNumber={inputs.nodes}
+          closedData={boundaryCondition}
+        />
         <h3 className="text-lg font-semibold mb-2">Parámetros Iniciales</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="flex items-center gap-2">
             <label htmlFor="E0" className="block font-semibold w-1/3">
-            Amplitud (
+              Amplitud (
               <span className="italic">
                 E<sub>0</sub>
               </span>
@@ -164,7 +165,7 @@ const WaveControls = () => {
           </div>
           <div className="flex items-center gap-2">
             <label htmlFor="nodes" className="block font-semibold w-1/3">
-              Armonicos:
+              Armonicos a calcular:
             </label>
             <input
               type="number"
@@ -177,7 +178,9 @@ const WaveControls = () => {
         </div>
 
         <div className="mt-4">
-          <h3 className="text-lg font-semibold mb-2">Condiciones de Frontera</h3>
+          <h3 className="text-lg font-semibold mb-2">
+            Condiciones de Frontera
+          </h3>
           <div className="flex items-center gap-4">
             <label>
               <input
@@ -211,12 +214,297 @@ const WaveControls = () => {
             </label>
           </div>
         </div>
-
+        <button>Actualizar Gráfica</button>
         <div className="mt-4">
           <h3 className="text-lg font-semibold mb-2">Datos</h3>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {harmonics.map(({ n, k, w, lambda, E0, B0 }) => (
-              <div key={n} className="text-sm font-mono bg-gray-50 p-4 rounded-lg shadow-sm">
+              <div
+                key={n}
+                className="text-sm font-mono bg-gray-50 p-4 rounded-lg shadow-sm"
+              >
+                <p>n = {n}</p>
+                <p>λ = {lambda.toFixed(2)}m</p>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: renderEquation(
+                      equations[boundaryCondition].E(k, w, E0)
+                    ),
+                  }}
+                />
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: renderEquation(
+                      equations[boundaryCondition].B(k, w, B0)
+                    ),
+                  }}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WaveControls;
+*/
+
+const WaveControls = () => {
+  let k, w, lambda, E0, B0;
+  const [boundaryCondition, setBoundaryCondition] = useState("cerrada-cerrada");
+
+  const equations = {
+    "cerrada-cerrada": {
+      factor: 2, // Factor multiplicativo para esta condición
+      E: (k, w, E0) =>
+        `E(x,t) = ${2 * E0} · Sin(${k.toFixed(2)} · x) · Cos(${w.toFixed(
+          2
+        )} · t)`,
+      B: (k, w, B0) =>
+        `B(x,t) = ${2 * B0} · Sin(${w.toFixed(2)} · t) · Cos(${k.toFixed(
+          2
+        )} · x)`,
+    },
+    "abierta-abierta": {
+      factor: 2, // Factor multiplicativo para esta condición
+      E: (k, w, E0) =>
+        `E(x,t) = ${2 * E0} · Cos(${k.toFixed(2)} · x) · Cos(${w.toFixed(
+          2
+        )} · t)`,
+      B: (k, w, B0) =>
+        `B(x,t) = ${2 * B0} · Sin(${k.toFixed(2)} · x) · Sin(${w.toFixed(
+          2
+        )} · t)`,
+    },
+    "abierta-cerrada": {
+      factor: 1, // Sin multiplicador adicional
+      E: (k, w, E0) =>
+        `E(x,t) = ${E0} · Cos(${k.toFixed(2)} · x) · Cos(${w.toFixed(2)} · t)`,
+      B: (k, w, B0) =>
+        `B(x,t) = ${B0} · Sin(${k.toFixed(2)} · x) · Cos(${w.toFixed(2)} · t)`,
+    },
+  };
+
+  const [inputs, setInputs] = useState({
+    L: "10", // cadena vacía en lugar de 1
+    v: "10", // cadena vacía en lugar de 1
+    amplitude: "",
+    nodes: "1", // cadena vacía en lugar de 1
+    E0: "",
+  });
+
+  const getEvaluableFormulas = (boundaryCondition, k, w, E0, B0) => {
+    const condition = equations[boundaryCondition];
+
+    // Extraer las fórmulas
+    const electricFormula = condition
+      .E(k, w, E0)
+      .replace(/E\(x,t\) = /, "")
+      .replace(/Sin/g, "sin")
+      .replace(/Cos/g, "cos")
+      .replace(/·/g, "*");
+    const magneticFormula = condition
+      .B(k, w, B0)
+      .replace(/B\(x,t\) = /, "")
+      .replace(/Sin/g, "sin")
+      .replace(/Cos/g, "cos")
+      .replace(/·/g, "*");
+
+    return { electric: electricFormula, magnetic: magneticFormula };
+  };
+
+  const [harmonics, setHarmonics] = useState([]);
+
+  // Calcular constantes dinámicas para cada nodo
+  const calculateHarmonics = () => {
+    const { L, v, nodes, E0 } = inputs;
+    const newHarmonics = [];
+    let electricFormula = "";
+    let magneticFormula = "";
+
+    for (let n = 1; n <= nodes; n++) {
+      switch (boundaryCondition) {
+        case "cerrada-cerrada":
+        case "abierta-abierta":
+          k = (Math.PI * n) / L;
+          break;
+        case "abierta-cerrada":
+          k = ((2 * n - 1) * Math.PI) / (2 * L);
+          break;
+        default:
+          k = 0;
+      }
+
+      w = k * v;
+      lambda = (2 * L) / n;
+      B0 = E0 / v;
+
+      // Obtener las fórmulas evaluables
+      const formulas = getEvaluableFormulas(boundaryCondition, k, w, E0, B0);
+
+      // Actualizar fórmulas para graficar
+      if (n === nodes) {
+        electricFormula = formulas.electric;
+        magneticFormula = formulas.magnetic;
+      }
+
+      newHarmonics.push({ n, k, w, lambda, E0, B0 });
+    }
+
+    setGraphData({ E: electricFormula, B: magneticFormula });
+    setHarmonics(newHarmonics);
+  };
+
+  // Recalcular armonías cuando cambien inputs o frontera
+  useEffect(() => {
+    calculateHarmonics();
+  }, [inputs, boundaryCondition]);
+
+  // Manejo de cambios en inputs
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    // Si el valor es una cadena vacía, dejamos el campo vacío
+    setInputs({
+      ...inputs,
+      [name]: value === "" ? "" : Number(value),
+    });
+  };
+
+  // Manejo de cambio de condiciones de frontera
+  const handleBoundaryChange = (e) => {
+    setBoundaryCondition(e.target.value);
+  };
+
+  // Función para renderizar ecuaciones usando KaTeX
+  const renderEquation = (equation) => {
+    return katex.renderToString(equation, {
+      throwOnError: false,
+    });
+  };
+
+  const [graphData, setGraphData] = useState({
+    E: "cola",
+    B: "bola",
+  });
+
+  return (
+    <div className="container mx-auto p-4">
+      <div className="bg-white rounded-lg shadow-md p-6 space-y-6">
+        <Graph3d
+          formulas={{
+            electric: graphData.E,
+            magnetic: graphData.B,
+          }}
+          cavityLenght={inputs.L}
+          harmonicNumber={inputs.nodes}
+          closedData={boundaryCondition}
+        />
+        <h3 className="text-lg font-semibold mb-2">Parámetros Iniciales</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="flex items-center gap-2">
+            <label htmlFor="E0" className="block font-semibold w-1/3">
+              Amplitud (
+              <span className="italic">
+                E<sub>0</sub>
+              </span>
+              ):
+            </label>
+            <input
+              type="number"
+              name="E0"
+              onChange={handleInputChange}
+              className="border rounded px-2 py-1 w-2/3 text-sm"
+            />
+            <span className="ml-2">V/m</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <label htmlFor="L" className="block font-semibold w-1/3">
+              Longitud de la Cavidad:
+            </label>
+            <input
+              type="number"
+              name="L"
+              value={inputs.L}
+              onChange={handleInputChange}
+              className="border rounded px-2 py-1 w-2/3 text-sm"
+            />
+            <span className="ml-2">m</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <label htmlFor="v" className="block font-semibold w-1/3">
+              Velocidad:
+            </label>
+            <input
+              type="number"
+              name="v"
+              value={inputs.v}
+              onChange={handleInputChange}
+              className="border rounded px-2 py-1 w-2/3 text-sm"
+            />
+            <span className="ml-2">m/s</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <label htmlFor="nodes" className="block font-semibold w-1/3">
+              Armonicos a calcular:
+            </label>
+            <input
+              type="number"
+              name="nodes"
+              value={inputs.nodes}
+              onChange={handleInputChange}
+              className="border rounded px-2 py-1 w-2/3 text-sm"
+            />
+          </div>
+        </div>
+
+        <div className="mt-4">
+          <h3 className="text-lg font-semibold mb-2">
+            Condiciones de Frontera
+          </h3>
+          <div className="flex items-center gap-4">
+            <label>
+              <input
+                type="radio"
+                name="boundary"
+                value="cerrada-cerrada"
+                checked={boundaryCondition === "cerrada-cerrada"}
+                onChange={handleBoundaryChange}
+              />
+              Cerrada-Cerrada
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="boundary"
+                value="abierta-abierta"
+                checked={boundaryCondition === "abierta-abierta"}
+                onChange={handleBoundaryChange}
+              />
+              Abierta-Abierta
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="boundary"
+                value="abierta-cerrada"
+                checked={boundaryCondition === "abierta-cerrada"}
+                onChange={handleBoundaryChange}
+              />
+              Abierta-Cerrada
+            </label>
+          </div>
+        </div>
+        <div className="mt-4">
+          <h3 className="text-lg font-semibold mb-2">Datos</h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {harmonics.map(({ n, k, w, lambda, E0, B0 }) => (
+              <div
+                key={n}
+                className="text-sm font-mono bg-gray-50 p-4 rounded-lg shadow-sm"
+              >
                 <p>n = {n}</p>
                 <p>λ = {lambda.toFixed(2)}m</p>
                 <p
